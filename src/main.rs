aaa6eafd8f9a40c66706fb8be13551c8b417c94f
scientiac <thescientiac@protonmail.ch>
2023-06-24 20:18:59 +0545

    Hello world on bare metal.

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+static GREET: &[u8] = b"Hi Pi!";
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in GREET.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x12;
+        }
+    }
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
